b735da559a7d314f7517d4ef786750c5cebee93b
thanhviD <117325015+thanhviD@users.noreply.github.com>
2022-12-03 21:20:13 +0700

    Update Apriori_T.py

--- Python/Apriori_T.py
+++ Python/Apriori_T.py
@@ -1,51 +1,47 @@
 import time
 
-def CandidateItemsets(Fk_1):  # Fk_1 : frequent k-1-itemsets
-    Ck = set()
-    for i in Fk_1:
-        for j in Fk_1:
-            if len(i.union(j)) == len(i) + 1:
-                Ck.add(i.union(j))
-    return Ck
+class Apriori:
+    def __init__(self, data, minSup) -> None:
+        self.data = data
+        self.minSup = minSup
+        self.result = []
+    def CandidateItemsets(self, Fk_1):  # Fk_1 : frequent k-1-itemsets
+        Ck = set()
+        for i in Fk_1:
+            for j in Fk_1:
+                if len(i.union(j)) == len(i) + 1:
+                    Ck.add(i.union(j))
+        return Ck    
+    def freq_1(self, minsup=0.4):
+        F1 = set()
+        for i in self.data:
+            for item in i:
+                if self.sup({item}) >= self.minSup:
+                    F1.add(frozenset({item}))
+        return F1
+    def sup(self, i):
+        count = 0
+        for j in self.data:
+            if i.issubset(j):
+                count += 1
+        return count / len(self.data)
+    
+    def Apriori(self):  
+        F1 = self.freq_1()
+        result = [F1]  # F : a set of frequent itemsets
+        k = 2
+        while len(result[k - 2]) > 0:
+            # print(k-2)
+            Ck = self.CandidateItemsets(result[k - 2])  # Ck : candidate k-itemsets
+            #print('Ck',Ck)
+            Fk = set()  # Fk : frequent k-itemsets
+            for X in Ck:
+                if self.sup(X) >= self.minSup:
+                    Fk.add(X)
+            result.append(Fk)
+            k += 1
+        return result
 
-# Create 1-itemsets
-def freq_1(D, minsup=0.4):
-    F1 = set()
-    for i in D:
-        for item in i:
-            if sup({item}, D) >= minsup:
-                F1.add(frozenset({item}))
-    return F1
-
-
-def Apiori(D, minsup):  
-    F1 = freq_1(D)
-    F = [F1]  # F : a set of frequent itemsets
-    k = 2
-    while len(F[k - 2]) > 0:
-        # print(k-2)
-        Ck = CandidateItemsets(F[k - 2])  # Ck : candidate k-itemsets
-        #print('Ck',Ck)
-        Fk = set()  # Fk : frequent k-itemsets
-        for X in Ck:
-            if sup(X, D) >= minsup:
-                Fk.add(X)
-        F.append(Fk)
-        k += 1
-    return F
-   
-    # ===============
-
-
-
-
-def sup(i, D):
-    count = 0
-    for j in D:
-        if i.issubset(j):
-            count += 1
-    res = count / len(D)
-    return res
 
 def main():
     D = [{1, 3, 4}, {2, 3, 5}, {1, 2, 3, 5}, {2, 5}, {1, 2, 3, 5}]
@@ -62,17 +58,19 @@ def main():
               {0, 1, 2, 5}]
     minsup = 0.4
     start = time.time()
-    F = Apiori(D, minsup)
-   
+    F = Apriori(D, minsup)
+    F = F.Apriori()
     for i in range(len(F)):
         S = []
         for j in F[i]:
             S.append(list(j))
         print(str(i + 1) + "-itemsets: " + str(S))
+    print(S)
+
     end = time.time()
     elapsed = (end - start) * 1000
-    print("Time: %f ms" % elapsed)
-    
+    print("Time taken: %f ms" % elapsed)
+    #print('check support:', F.CandidateGeneration(F.freq_1(D)))
 if __name__ == "__main__":
     main()
     
