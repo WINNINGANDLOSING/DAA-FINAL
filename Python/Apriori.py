631e6683d7bcaa942aca6b76ee608abdd6418098
kataowl <khanhtanfts@gmail.com>
2022-12-03 23:09:03 +0700

    finalize apriori

--- Python/Apriori.py
+++ Python/Apriori.py
@@ -1,137 +1,67 @@
-# https://japp.io/machine-learning/apriori-algorithm-program-in-python-from-scratch/
+class Apriori:
+    # the class the group all the function for the aprioriTID algorithm
+    
+    def __init__(self, horizontal_database: list[set[int]], minsup_percentage: float) -> None:
+        # constuctor that take in a horizontal database as a list of sets of int, and minumun support in percentage.
+        self.database = horizontal_database
+        self.minsup = int(minsup_percentage * len(horizontal_database))
 
-def apriori():
-    from collections import Counter
-    c = Counter()
-    for i in init:
-        for d in data:
-            if(i in d[1]):
-                c[i]+=1
-    print("C1:")
-    for i in c:
-        print(str([i])+": "+str(c[i]))
-    print()
-    l = Counter()
-    for i in c:
-        if(c[i] >= s):
-            l[frozenset([i])]+=c[i]
-    print("L1:")
-    for i in l:
-        print(str(list(i))+": "+str(l[i]))
-    print()
-    pl = l
-    pos = 1
-    for count in range (2,1000):
-        nc = set()
-        temp = list(l)
-        for i in range(0,len(temp)):
-            for j in range(i+1,len(temp)):
-                t = temp[i].union(temp[j])
-                if(len(t) == count):
-                    nc.add(temp[i].union(temp[j]))
-        nc = list(nc)
-        c = Counter()
-        for i in nc:
-            c[i] = 0
-            for q in data:
-                temp = set(q[1])
-                if(i.issubset(temp)):
-                    c[i]+=1
-        print("C"+str(count)+":")
-        for i in c:
-            print(str(list(i))+": "+str(c[i]))
-        print()
-        l = Counter()
-        for i in c:
-            if(c[i] >= s):
-                l[i]+=c[i]
-        print("L"+str(count)+":")
-        for i in l:
-            print(str(list(i))+": "+str(l[i]))
-        print()
-        if(len(l) == 0):
-            break
-        pl = l
-        pos = count
-    print("Result: ")
-    print("L"+str(pos)+":")
-    for i in pl:
-        print(str(list(i))+": "+str(pl[i]))
-    print()
+    def generate_candidate(self, prelevel_frequent_itemsets: set) -> set:  
+        # generate next level (k) set of itemsets based on the previous one (k-1) itemsets.
+        # input: set of itemsets that has k-1 items
+        # output: set of itemsets that has k items
+        Ck = set()
+        for i in prelevel_frequent_itemsets:
+            for j in prelevel_frequent_itemsets:
+                if len(i.union(j)) == len(i) + 1:
+                    Ck.add(i.union(j))
+        return Ck   
 
-data = [
-        ['T100',['I1','I2','I5']],
-        ['T200',['I2','I4']],
-        ['T300',['I2','I3']],
-        ['T400',['I1','I2','I4']],
-        ['T500',['I1','I3']],
-        ['T600',['I2','I3']],
-        ['T700',['I1','I3']],
-        ['T800',['I1','I2','I3','I5']],
-        ['T900',['I1','I2','I3']]
-        ]
-init = []
-for i in data:
-    for q in i[1]:
-        if(q not in init):
-            init.append(q)
-init = sorted(init)
+    def find_frequent_1_itemsets(self):
+        # find itemsets that has only 1 item and meet the minimun support
+        F1 = set()
+        for i in self.database:
+            for item in i:
+                if self.find_support({item}) >= self.minsup:
+                    F1.add(frozenset({item}))
+        return F1
+
+    def find_support(self, i):
+        # find support of the given itemset
+        count = 0
+        for j in self.database:
+            if i.issubset(j):
+                count += 1
+        return count
+    
+    # ALGORITHM FUNCTION
+    def run(self):
+        # run apriori algorithm to find the frequent itemsets
+        # return the list of frequent itemset found by apriori algorithm
+
+        F1 = self.find_frequent_1_itemsets()
+        result = [F1]  
+        k = 2
+        while len(result[k - 2]) > 0:
+            Ck = self.generate_candidate(result[k - 2])  # candidate k-itemsets
+            Fk = set()  
+            for X in Ck:
+                if self.find_support(X) >= self.minsup:
+                    Fk.add(X)
+            result.append(Fk)
+            k += 1
+        return result
 
 
-sp = 0.4
-s = int(sp*len(init))
-apriori()
-
-
-# from itertools import combinations
-# for l in pl:
-#     c = [frozenset(q) for q in combinations(l,len(l)-1)]
-#     mmax = 0
-#     for a in c:
-#         b = l-a
-#         ab = l
-#         sab = 0
-#         sa = 0
-#         sb = 0
-#         for q in data:
-#             temp = set(q[1])
-#             if(a.issubset(temp)):
-#                 sa+=1
-#             if(b.issubset(temp)):
-#                 sb+=1
-#             if(ab.issubset(temp)):
-#                 sab+=1
-#         temp = sab/sa*100
-#         if(temp > mmax):
-#             mmax = temp
-#         temp = sab/sb*100
-#         if(temp > mmax):
-#             mmax = temp
-#         print(str(list(a))+" -> "+str(list(b))+" = "+str(sab/sa*100)+"%")
-#         print(str(list(b))+" -> "+str(list(a))+" = "+str(sab/sb*100)+"%")
-#     curr = 1
-#     print("choosing:", end=' ')
-#     for a in c:
-#         b = l-a
-#         ab = l
-#         sab = 0
-#         sa = 0
-#         sb = 0
-#         for q in data:
-#             temp = set(q[1])
-#             if(a.issubset(temp)):
-#                 sa+=1
-#             if(b.issubset(temp)):
-#                 sb+=1
-#             if(ab.issubset(temp)):
-#                 sab+=1
-#         temp = sab/sa*100
-#         if(temp == mmax):
-#             print(curr, end = ' ')
-#         curr += 1
-#         temp = sab/sb*100
-#         if(temp == mmax):
-#             print(curr, end = ' ')
-#         curr += 1
-#     print()
-#     print()
+def main():
+    horizontal_database =  [{1, 3, 4}, 
+                            {2, 3, 5}, 
+                            {1, 2, 3, 5}, 
+                            {2, 5}, 
+                            {1, 2, 3, 5}]
+    apriori = Apriori(horizontal_database, 40/100)
+    print(apriori.minsup)
+    print(list(apriori.run()))
+    
+if __name__ == "__main__":
+    main()
