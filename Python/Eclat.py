88c694804d2bf656fe5b0ca6ba9badaa81a7dd19
WINNINGANDLOSING <106044627+WINNINGANDLOSING@users.noreply.github.com>
2022-12-03 18:37:56 +0700

    Update Eclat.py
    Thêm comment vô, ai rảnh thì thêm document nha

--- Python/Eclat.py
+++ Python/Eclat.py
@@ -1,5 +1,6 @@
 import time
 true_res = []
+# function to union two itemsets ('1', '5') and ('3', '5') return ['1','3','5']. However, this only return combined itemsets whose length is larger than it should be (depending on k) and duplicate itemsets
 def _list(i,j):
         temp_list = []
         temp_list.append(i)
@@ -10,7 +11,7 @@ def _list(i,j):
                 if x_ not in final_list:
                     final_list.append(x_)
         return final_list
-
+# function to return only the unique itemsets and itemsets whose length is approriate based on k
 def sorted_res(list,max_length):
         my_list = []
         true_list = []
@@ -20,7 +21,7 @@ def sorted_res(list,max_length):
             if x not in true_list and len(x)==max_length+1:
                 true_list.append(sorted(x))
         return true_list
-        
+# function to print the result
 def printRes(res):
     for i in res:
         print(i)
@@ -32,6 +33,7 @@ def eclat(R, minsup):
     res = []
     for i in R:
         max_length = (len(i))
+    # 1-itemset   
     if max_length==1:
         itemset_1 = []
         for i in R:
@@ -44,20 +46,22 @@ def eclat(R, minsup):
     for i in R.copy():
         for j in R.copy():
             if i != j:
-                temp_set = R[i]&R[j]
-                if len(temp_set) >=minsup and ([i,j]) not in check_repeat:
-                    #print("i", i, "j", j,"R[i]", R[i], "R[j]",R[j], "R[i] & R[j]", temp_set, len(temp_set))
+                temp_set = R[i]&R[j] # union of two tidsets
+                if len(temp_set) >=minsup and ([i,j]) not in check_repeat: # if tid(x) >= minsup
                     final_list = _list(i,j)
                     final_list = sorted(final_list)
                     res.append(final_list)
-                    if(len(tuple(final_list)))==max_length+1:
-                        R_[tuple(final_list)] = {st for st in temp_set}
-                    check_repeat.append([i,j])
+                    
+                    if(len(tuple(final_list)))==max_length+1: # only if the length of k itemset is 1 + length of k-1 itemset
+                        R_[tuple(final_list)] = {st for st in temp_set} # Get the itemset-tid pair 
+                    check_repeat.append([i,j]) #check if two itemsets are already mined
                     check_repeat.append([j,i])
     
     res = sorted_res(res,max_length)
     true_res.append(res)
+    # Recursively mine the table in k+1 til no more itemset-tid pairs can be created
     eclat(R_,minsup)
+    
     
 def main(): 
     """Main function."""
